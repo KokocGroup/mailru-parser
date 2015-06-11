208439665634b629fe19798b5498e4528a7702dc
Galkin Sergey <galkinsergeyv@gmail.com>
2015-06-11 18:32:25 +0300

    formats for 2.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Parse html content of Mail.ru",
     version=VERSION,
     url='https://github.com/KokocGroup/mailru-parser',
-    download_url='https://github.com/KokocGroup/mailru-parser/tarball/v{}'.format(VERSION),
+    download_url='https://github.com/KokocGroup/mailru-parser/tarball/v{0}'.format(VERSION),
     packages=['mailru_parser'],
     install_requires=[
 #         'pyquery==1.2.9',
